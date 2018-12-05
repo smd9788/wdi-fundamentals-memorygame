39ab09aca6de75d3272113438354fa12f80344b2
Stephen Dicicco <stephendicicco@Stephens-MacBook-Pro.local>
2018-12-05 16:48:33 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
-console.log("User flipped a " + cardOne);
-console.log("User flipped a " + cardThree);
+console.log("User flipped " + cardsInPlay [0]);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
