a92c2e06c35350274dfd1dc8c7e0762d00e009cf
Stephen Dicicco <stephendicicco@Stephens-MacBook-Pro.local>
2018-12-06 14:28:14 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+		},
+		{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonnds.png'
+		},
+		{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-diamonnds.png'
+		},
+		{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonnds.png'
+		}
+		];
+
 var cardsInPlay = [];
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,12 +29,15 @@ var checkForMatch = function() {
 	}
 }
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length > 1) {
 		checkForMatch();
 	}
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 flipCard(0);
 flipCard(2);
+
