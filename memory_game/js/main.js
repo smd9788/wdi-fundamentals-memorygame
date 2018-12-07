728da2805f7c7be316ed3f43606f4fc106951e17
Stephen Dicicco <stephendicicco@Stephens-MacBook-Pro.local>
2018-12-07 12:17:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,21 +6,25 @@ var cards = [{
 		{
 		rank: 'queen',
 		suit: 'diamonds',
-		cardImage: 'images/queen-of-diamonnds.png'
+		cardImage: 'images/queen-of-diamonds.png'
 		},
 		{
 		rank: 'king',
 		suit: 'hearts',
-		cardImage: 'images/king-of-diamonnds.png'
+		cardImage: 'images/king-of-hearts.png'
 		},
 		{
 		rank: 'king',
 		suit: 'diamonds',
-		cardImage: 'images/king-of-diamonnds.png'
+		cardImage: 'images/king-of-diamonds.png'
 		}
 		];
 
+
+
 var cardsInPlay = [];
+
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -28,16 +32,34 @@ var checkForMatch = function() {
 		alert("sorry, try again!");
 	}
 }
-var flipCard = function(cardId) {
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src' , "images/back.png");
+    cardElement.setAttribute('data-Id' , i);
+    cardElement.addEventListener('click' , flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+var flipCard = function() {
+	var cardId = this.getAttribute('data-Id')
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src' , cards[cardId].cardImage);
 	if (cardsInPlay.length > 1) {
 		checkForMatch();
 	}
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+}
+var replay = function() {
+	var replayButton = document.getElementById('play-Again');
+	replayButton.addEventListener('click' , function(){
+		window.location.reload();
+	});
+	document.getElementById('play-Again').appendChild(replayButton);
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
+replay();
 
